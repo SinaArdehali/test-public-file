d2e25642939a6cad4a1b3c4c7c38fe3ee24fe9f8
sinaardehali <sina.ardehali@gmail.com>
2024-10-16 00:21:49 +0200

    Create custom-script.js

---
+++ custom-script.js
@@ -0,0 +1,20 @@
+< script >
+    document.addEventListener("DOMContentLoaded", function() {
+        // Sélectionner le bouton "S'inscrire"
+        var submitButton = document.querySelector('a[href="#submit-form"]');
+        var originalButtonText = submitButton.querySelector('.elButtonMain').textContent;
+
+        // Intercepter l'événement de clic sur le bouton "S'inscrire"
+        submitButton.addEventListener("click", function(event) {
+            event.preventDefault(); // Empêcher le comportement par défaut du bouton (la redirection)
+
+            var phoneNumber = document.querySelector('input[name="phone"]').value; // Sélectionner le champ de téléphone
+            console.log(phoneNumber)
+
+            // Afficher un message de chargement ou désactiver le bouton
+            submitButton.classList.add('disabled'); // Ajoutez une classe pour indiquer le chargement si nécessaire
+            submitButton.querySelector('.elButtonMain').textContent = "Vérification en cours...";
+
+        });
+    });
+</script>
