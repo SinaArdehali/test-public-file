f09a3c21b2143eefec6077e1325d8c26b9e1caeb
sinaardehali <sina.ardehali@gmail.com>
2024-10-16 00:36:19 +0200

    Update custom-script.js

--- custom-script.js
+++ custom-script.js
@@ -1,20 +1,30 @@
 <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        // Sélectionner le bouton "S'inscrire"
-        var submitButton = document.querySelector('a[href="#submit-form"]');
-        var originalButtonText = submitButton.querySelector('.elButtonMain').textContent;
+    document.addEventListener("DOMContentLoaded", function () {
+      // Sélectionner le bouton "S'inscrire"
+      var submitButton = document.querySelector('a[href="#submit-form"]');
+      if (!submitButton) {
+         console.error("Bouton 'S'inscrire' non trouvé.");
+         return;
+      }
 
-        // Intercepter l'événement de clic sur le bouton "S'inscrire"
-        submitButton.addEventListener("click", function(event) {
-            event.preventDefault(); // Empêcher le comportement par défaut du bouton (la redirection)
+      var originalButtonText = submitButton.querySelector('.elButtonMain').textContent;
+      if (!originalButtonText) {
+         console.error("Texte du bouton 'S'inscrire' non trouvé.");
+         return;
+      }
 
-            var phoneNumber = document.querySelector('input[name="phone"]').value; // Sélectionner le champ de téléphone
-            console.log(phoneNumber)
+      // Intercepter l'événement de clic sur le bouton "S'inscrire"
+      submitButton.addEventListener("click", function (event) {
+         event.preventDefault(); // Empêcher le comportement par défaut du bouton (la redirection)
 
-            // Afficher un message de chargement ou désactiver le bouton
-            submitButton.classList.add('disabled'); // Ajoutez une classe pour indiquer le chargement si nécessaire
-            submitButton.querySelector('.elButtonMain').textContent = "Vérification en cours...";
+         var phoneNumber = document.querySelector('input[name="phone"]').value; // Sélectionner le champ de téléphone
+         console.log(phoneNumber)
 
-        });
-    });
-</script>
+         // Afficher un message de chargement ou désactiver le bouton
+         submitButton.classList.add('disabled'); // Ajoutez une classe pour indiquer le chargement si nécessaire
+         // submitButton.querySelector('.elButtonMain').textContent = "Vérification en cours...";
+         originalButtonText.textContent = "Vérification en cours...";
+
+      });
+   }); <
+/script>
